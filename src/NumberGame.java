5138a197fc5c2afab209e08ff4692a0db0c97b32
aman0iz0back <dr.amans8932@gmail.com>
2024-05-05 23:26:27 +0530

    Task-1 Number Game

--- src/NumberGame.java
+++ src/NumberGame.java
@@ -2,7 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class NumberGame {
-    int c = 1;
+    int attempts = 1;
 
     public void NumberGenerate() {
         Scanner sc = new Scanner(System.in);
@@ -13,24 +13,24 @@ public class NumberGame {
         Random random = new Random();
         int f = 0;
         int randomNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
-        c = 1;
-        while (c <= 5) {
+        attempts = 1;
+        while (attempts <= 5) {
             int number = acceptNumber();
             if (randomNumber == number) {
                 f = 1;
                 System.out.println("Correct Guess");
-                if (c == 1) {
-                    System.out.println("You guessed the number in " + c + " attempt.\n");
+                if (attempts == 1) {
+                    System.out.println("You guessed the number in " + attempts + " attempt.\n");
                     break;
                 } else {
-                    System.out.println("You guessed the number in " + c + " attempts.\n");
+                    System.out.println("You guessed the number in " + attempts + " attempts.\n");
                     break;
                 }
             } else if (randomNumber < number) {
-                c++;
+                attempts++;
                 System.out.println("Guess too high");
             } else {
-                c++;
+                attempts++;
                 System.out.println("Guess too low");
             }
         }
